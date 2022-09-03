0fe4214883b5634817693e94be1b3b1bb461e7d3
nazmuldip <nazmuldip21@gmail.com>
2022-09-03 23:39:52 +0600

    update html loading news category and display news category added

--- js/news.js
+++ js/news.js
@@ -1,4 +1,20 @@
-fetch('https://openapi.programming-hero.com/api/news/categories')
-    .then(res => res.json())
-    .then(data => console.log(data.data.news_category))
-    .catch(error => console.log(error));
+// loading news category
+const loadNewsCategory = () => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayNewsCategory(data.data.news_category))
+        .catch(error => console.log(error));
+}
+// displaying news category
+const displayNewsCategory = catagorys => {
+    const catagoryContainer = document.getElementById('news-category');
+    catagorys.forEach(catagory => {
+
+        const creatDiv = document.createElement('div');
+        creatDiv.innerHTML = `
+        <a onclick="loadNewsDetails('${catagory.category_id}')" href="#news-category" class="text-decoration-none">${catagory.category_name}</a>
+        `;
+        catagoryContainer.appendChild(creatDiv);
+    })
+}
