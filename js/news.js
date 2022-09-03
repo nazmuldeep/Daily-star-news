34c039a70c6cbce6d32c8c09b124727888660a5a
nazmuldip <nazmuldip21@gmail.com>
2022-09-03 20:19:22 +0600

    news category fetching

--- js/news.js
+++ js/news.js
@@ -1,1 +1,4 @@
-console.log('connected to ')
+fetch('https://openapi.programming-hero.com/api/news/categories')
+    .then(res => res.json())
+    .then(data => console.log(data.data.news_category))
+    .catch(error => console.log(error));
